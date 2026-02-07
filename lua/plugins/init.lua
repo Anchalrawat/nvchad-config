1095c09c6dc1ab13e9cdd05243fef7758fc635bb
Anchal Rawat <101033524+Anchalrawat@users.noreply.github.com>
2026-02-07 12:55:01 +0530

    added colors

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -28,4 +28,23 @@ return {
   	},
   },
 
+  {
+    "norcalli/nvim-colorizer.lua",
+    event = "BufReadPre",
+    config = function()
+      require("colorizer").setup({
+        "*", -- Highlight all files
+        css = { rgb_fn = true }, -- Enable parsing rgb(...) functions in css
+        html = { names = false } -- Disable named colors in HTML
+      }, {
+        RGB = false, -- #RGB hex codes
+        RRGGBB = false, -- #RRGGBB hex codes
+        names = false, -- Disable "Name" codes like Blue
+        RRGGBBAA = false, -- #RRGGBBAA hex codes
+        rgb_fn = true, -- CSS rgb() and rgba() functions
+        hsl_fn = false, -- CSS hsl() and hsla() functions
+        mode = "background", -- Set the display mode
+      })
+    end,
+  },
 }
